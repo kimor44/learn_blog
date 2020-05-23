05cb5f88e8eb05c7a0f8ba15223e72adc77f69c1
Julien Guibert <julien.guib@gmail.com>
2020-05-23 15:52:20 +0200

    End OC courses part I : Initiatiez vous à RoR

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     @number = 10
   end
+
+  def test
+    @test = "Ici une phrase"
+  end
+
+  def castor
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get 'test' => 'pages#test'
+  get 'castor' => 'pages#castor'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
