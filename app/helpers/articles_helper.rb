d4126e7207e360570ceba297f4067455519e625e
Julien Guibert <julien.guib@gmail.com>
2020-08-13 12:10:38 +0200

    Add 'Prev' and 'Next' buttons on article pages (Used helpers)

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def prev_article
+    Article.where("id < #{params[:id]}").maximum(:id)
+  end
+
+  def next_article
+    Article.where("id > #{params[:id]}").minimum(:id)
+  end
 end
