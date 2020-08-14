959ebb244ded33d41e0cd62c06bac9e8e8b932dd
Julien Guibert <julien.guib@gmail.com>
2020-08-14 14:57:38 +0200

    Spec::Model#article Add specs for validates attributes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  validates :title, presence: { message: 'title is mandatory!' }
+  validates :body, presence: { message: 'body content is mandatory!' }
+
   accepts_nested_attributes_for :comments
 
   scope :prev_article, ->(article_id) { where("id < ?", article_id).maximum(:id) }
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.create!(email: 'coucou@gmail.com', admin: true, password: 'azerty') }
+  subject(:article) { Article.new }
+
+  before do
+    article.update(title: 'sdfdfs', body: 'Some text long text.....', user_id: user.id)
+  end
+
+  context 'create an article with title and body' do
+    it 'is possible' do
+      expect(article.valid?).to be true
+    end
+  end
+
+  context 'create an article without title' do
+    it 'is NOT possible' do
+      article.title = ""
+      expect(article.valid?).to be false
+      expect(article.errors[:title].any?).to be true
+      expect(article.errors[:title].first).to eq "title is mandatory!"
+    end
+  end
+
+  context 'create an article without body' do
+    it 'is NOT possible' do
+      article.body = ""
+      expect(article.valid?).to be false
+      expect(article.errors[:body].any?).to be true
+      expect(article.errors[:body].first).to eq "body content is mandatory!"
+    end
+  end
 end
