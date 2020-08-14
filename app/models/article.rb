33a20e580d9da89dbf5c9ea1cdcd3afabb52c470
Julien Guibert <julien.guib@gmail.com>
2020-08-14 17:59:57 +0200

    Spec::Model/Article Add a validate when user create an article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,16 @@ class Article < ApplicationRecord
 
   validates :title, presence: { message: 'title is mandatory!' }
   validates :body, presence: { message: 'body content is mandatory!' }
+  validate :user_must_be_admin
 
   accepts_nested_attributes_for :comments
 
   scope :prev_article, ->(article_id) { where("id < ?", article_id).maximum(:id) }
   scope :next_article, ->(article_id) { where("id > ?", article_id).minimum(:id) }
+
+  def user_must_be_admin
+    if !user.admin
+      self.errors.add(:user_not_admin, 'user must be admin')
+    end
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -31,4 +31,14 @@ RSpec.describe Article, type: :model do
       expect(article.errors[:body].first).to eq "body content is mandatory!"
     end
   end
+
+  context 'create an article when user is not admin' do
+    it 'is NOT possible' do
+      user.update(admin: false)
+      article2 = Article.create(title: 'un titre', body: 'Lorem ipsum dolor sit amet', user_id: user.id)
+      expect(article2.valid?).to be false
+      expect(article2.errors[:user_not_admin].any?).to be true
+      expect(article2.errors[:user_not_admin].first).to eq 'user must be admin'
+    end
+  end
 end
